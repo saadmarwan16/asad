41232c22e821c54328b0e6cc1b41445f04516838
saadmarwan16 <saadmarwan16@gmail.com>
2024-01-08 21:32:23 +0300

    Added layout and some placeholder texts for the about and aims pages

--- src/app/aims/page.tsx
+++ src/app/aims/page.tsx
@@ -1,4 +1,6 @@
 import AboutAimsWrapper from "@asad/lib/components/AboutAimsWrapper";
+import Image from "next/image";
+import styles from "@asad/styles/shared/aboutaims.module.css";
 
 const Aims = () => {
   return (
@@ -8,7 +10,62 @@ const Aims = () => {
       url="/images/aims-large.png"
       alt="Aims"
     >
-      <h4>Aims page is coming soon</h4>
+      <div id={styles.container}>
+        <p id={styles.description} className="mb-12">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos
+          quibusdam accusamus maxime laboriosam iste commodi. Est adipisci
+          tempore, vero officia voluptatum perferendis quasi necessitatibus
+          quisquam eaque pariatur rerum neque possimus. Lorem ipsum dolor sit
+          amet consectetur adipisicing elit. Tenetur, dolorum adipisci! In iure
+          quasi, ut a iusto quaerat veniam officiis recusandae veritatis
+          blanditiis quis laborum. Aspernatur quis exercitationem veritatis
+          incidunt.
+        </p>
+        <div id={styles.imageOne}>
+          <Image
+            src="/images/aims-large.png"
+            alt="About"
+            width={400}
+            height={500}
+            className="aspect-video h-full w-full"
+          />
+        </div>
+        <div id={styles.contentOne} className="mb-12">
+          <h5>This is title 1</h5>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Blanditiis
+            nobis earum dolor velit. Fugit pariatur voluptates magni sequi cum
+            ratione. Accusamus reiciendis delectus exercitationem natus dolorem,
+            consequuntur adipisci. Consectetur, alias. Lorem ipsum dolor sit,
+            amet consectetur adipisicing elit. Voluptatum sunt dolorum incidunt
+            officia nostrum doloribus deleniti? Fuga odio quod veniam, nobis rem
+            dolorem, perferendis dolorum accusamus blanditiis, illo provident
+            corporis!
+          </p>
+        </div>
+        <div id={styles.contentTwo}>
+          <h5>This is title 2</h5>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Blanditiis
+            nobis earum dolor velit. Fugit pariatur voluptates magni sequi cum
+            ratione. Accusamus reiciendis delectus exercitationem natus dolorem,
+            consequuntur adipisci. Consectetur, alias. Lorem ipsum dolor sit,
+            amet consectetur adipisicing elit. Voluptatum sunt dolorum incidunt
+            officia nostrum doloribus deleniti? Fuga odio quod veniam, nobis rem
+            dolorem, perferendis dolorum accusamus blanditiis, illo provident
+            corporis!
+          </p>
+        </div>
+        <div id={styles.imageTwo}>
+          <Image
+            src="/images/aims-large.png"
+            alt="About"
+            width={400}
+            height={500}
+            className="aspect-video h-full w-full"
+          />
+        </div>
+      </div>
     </AboutAimsWrapper>
   );
 };
