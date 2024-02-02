c3c821fe6558ef54bb19bc78b25818c8115a8296
saadmarwan16 <saadmarwan16@gmail.com>
2024-02-02 08:25:41 +0300

    Created a static add executive page

--- src/app/admin/executives/new/page.tsx
+++ src/app/admin/executives/new/page.tsx
@@ -1,7 +1,49 @@
+import Button from "@asad/lib/ui/Button";
+import Input from "@asad/lib/ui/Input";
+import Select from "@asad/lib/ui/Select";
+import Textarea from "@asad/lib/ui/Textarea";
+import styles from "@asad/styles/admin/new_executive.module.css";
+import { FaImage } from "react-icons/fa";
+
 const AdminAddExecutive = () => {
   return (
     <div>
-      <h1>Admin add executive page is coming soon</h1>
+      <h4 className="mb-8 font-medium">Add executive memeber</h4>
+      <form id={styles.form}>
+        <div id={styles.name}>
+          <Input top="Name" placeholder="Enter name here" />
+        </div>
+        <div id={styles.role}>
+          <Select id="role" top="Role" defaultValue="">
+            <option value="" disabled>
+              Select a role
+            </option>
+            <option value="CEO">CEO</option>
+            <option value="CTO">CTO</option>
+            <option value="CFO">CFO</option>
+          </Select>
+        </div>
+        <div id={styles.duties}>
+          <Textarea
+            top="Duties"
+            placeholder="Write a short description of the executive member's role here"
+            rows={8}
+          />
+        </div>
+        <div id={styles.image} className="my-8">
+          <div className="flex cursor-pointer flex-col items-center justify-center gap-6 rounded-lg border-2 border-primary-100 px-10 py-12 hover:border-primary-200">
+            <FaImage className="text-6xl text-primary-200 sm:text-7xl md:text-8xl lg:text-9xl" />
+            <span className="text-lg font-medium text-primary-300 md:text-xl">
+              Add an image
+            </span>
+          </div>
+        </div>
+        <div id={styles.button}>
+          <Button type="submit" data-text="Add">
+            Add
+          </Button>
+        </div>
+      </form>
     </div>
   );
 };
---
+++ src/lib/ui/Select.tsx
@@ -0,0 +1,75 @@
+"use client";
+
+import { cva, type VariantProps } from "class-variance-authority";
+import type {
+  SelectHTMLAttributes,
+  FunctionComponent,
+  PropsWithChildren,
+} from "react";
+
+const select = cva("border-2 text-content-100", {
+  variants: {
+    variant: {
+      primary: `border-2 border-primary-100 bg-base-100 focus:border-primary-200 focus:outline-none`,
+      secondary: `border-2 border-secondary-100 bg-base-100 focus:border-secondary-200 focus:outline-none`,
+      ghost: `border-2 border-base-300 focus:border-base-400 focus:outline-none`,
+    },
+    width: {
+      fit: "w-fit",
+      full: "w-full",
+    },
+    size: {
+      sm: "rounded-md px-2 py-1 text-sm",
+      md: "rounded-lg px-4 py-2",
+      lg: "rounded-lg px-6 py-3 text-lg",
+    },
+  },
+  defaultVariants: {
+    variant: "primary",
+    width: "full",
+    size: "md",
+  },
+});
+
+type SelectVariantProps = VariantProps<typeof select>;
+
+interface SelectProps
+  extends Omit<SelectHTMLAttributes<HTMLSelectElement>, "size" | "width">,
+    SelectVariantProps,
+    PropsWithChildren {
+  top?: string;
+  bottom?: string;
+}
+
+const Select: FunctionComponent<SelectProps> = ({
+  variant,
+  width,
+  size,
+  top,
+  bottom,
+  children,
+  ...props
+}) => {
+  return (
+    <div className="flex flex-col gap-2">
+      {top && (
+        <label
+          htmlFor={props.id}
+          className="cursor-default select-none font-medium"
+        >
+          {top}
+        </label>
+      )}
+      <select {...props} className={select({ variant, width, size })}>
+        {children}
+      </select>
+      {bottom && (
+        <label className="cursor-default select-none font-medium text-red-800">
+          {bottom}
+        </label>
+      )}
+    </div>
+  );
+};
+
+export default Select;
